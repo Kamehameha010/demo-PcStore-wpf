3a18d1610c276b66b0e7d96cbc0a4c38e9ac0ec8
joel <mizukaze_010@hotmail.com>
2021-09-16 01:35:47 -0600

    add RelayCommand, ViewModelBase clasess, Sale View, also add Save,Update,Delete,Search in ProductViewModel class

---
+++ PcStore/Commands/RelayCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PcStore.Commands
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> _action;
+        private readonly Func<bool> _canExecute;
+
+        public RelayCommand(Action<object> action, Func<bool> canExecute)
+        {
+            _action = action;
+            _canExecute = canExecute;
+        }
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+        public bool CanExecute(object parameter) => (bool)(_canExecute?.Invoke());
+        public void Execute(object parameter = null)
+        {
+            Console.WriteLine(parameter);
+            _action(parameter);
+        }
+    }
+}
--- PcStore/ViewModels/ProductViewModel.cs
+++ PcStore/ViewModels/ProductViewModel.cs
@@ -1,20 +1,27 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using PcStore.Commands;
 using PcStore.Models;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace PcStore.ViewModels
 {
-    class ProductViewModel : INotifyPropertyChanged
+    class ProductViewModel : ViewModelBase
     {
 
         public ProductViewModel()
         {
+            _Product = new Product();
             LoadData();
-
+            _saveCommand = new RelayCommand(x => Save(), () => true);
+            _searchCommand = new RelayCommand(x => Search(x), () => true);
+            _updateCommand = new RelayCommand(x => Update(), () => true);
+            _deleteCommand = new RelayCommand(x => Delete(x), () => true);
         }
         #region Propeties
         private Product _Product;
@@ -29,36 +36,110 @@ namespace PcStore.ViewModels
         }
 
         //private ObservableCollection<Product> observable;
-        private List<Product> _Products;
-        public List<Product> Products
+        private ObservableCollection<Product> _products;
+        public ObservableCollection<Product> Products
         {
-            get => _Products;
+            get => _products;
             set
             {
-                _Products = value;
+                _products = value;
                 OnPropertyChanged(nameof(Products));
             }
         }
 
+        private readonly ICommand _saveCommand;
+        public ICommand SaveCommand { get => _saveCommand; }
+
+
+        private readonly ICommand _searchCommand;
+        public ICommand SearchCommand { get => _searchCommand; }
+
+        private readonly ICommand _updateCommand;
+        public ICommand UpdateCommand { get => _updateCommand; }
+        private readonly ICommand _deleteCommand;
+        public ICommand DeleteCommand { get => _deleteCommand; }
+
         #endregion Propeties
-
-
-        #region LoadData
         private void LoadData()
         {
             using var db = new Pc_StoreContext();
-            Products = db.Products.ToList();
+            Products = new ObservableCollection<Product>(db.Products.ToList());
         }
 
-        #endregion LoadData
+        #region Commands
 
-        #region INotifyPropertyChanged Implementation
-        public event PropertyChangedEventHandler PropertyChanged;
-        private void OnPropertyChanged(string propertyName)
+        public void Save()
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-        #endregion
+            try
+            {
+                using var db = new Pc_StoreContext();
+                db.Add(Product);
+                db.SaveChanges();
+                LoadData();
+                Product = new Product();
+            }
+            catch (System.Exception e)
+            {
+                MessageBox.Show("Error: {0}", e.InnerException.Message);
+            }
 
+        }
+
+        public void Search(object parameter)
+        {
+            try
+            {
+                using var db = new Pc_StoreContext();
+                var result = db.Products.Find(int.Parse(parameter.ToString()));
+                Product.ProductId = result.ProductId;
+                Product.Code = result.Code;
+                Product.Description = result.Description;
+                Product.Stock = result.Stock;
+                Product.Price = result.Price;
+
+            }
+            catch (System.Exception e)
+            {
+                MessageBox.Show("Error: {0}", e.InnerException.Message);
+            }
+        }
+
+        public void Update()
+        {
+            try
+            {
+                using var db = new Pc_StoreContext();
+                db.Entry(Product).State = EntityState.Modified;
+                db.SaveChanges();
+                LoadData();
+                //Product = new Product();
+            }
+            catch (System.Exception e)
+            {
+                MessageBox.Show("Error: {0}", e.Message);
+            }
+        }
+
+        public void Delete(object parameter)
+        {
+            try
+            {
+                var result = MessageBox.Show("Do you want delete?", "", MessageBoxButton.YesNo);
+
+                if (result == MessageBoxResult.Yes)
+                {
+                    using var db = new Pc_StoreContext();
+                    var oProduct = db.Products.Find(int.Parse(parameter.ToString()));
+                    db.Remove(oProduct);
+                    db.SaveChanges();
+                    LoadData();
+                }
+            }
+            catch (System.Exception e)
+            {
+                MessageBox.Show("Error: {0}", e.Message);
+            }
+        }
+        #endregion Commands
     }
 }
